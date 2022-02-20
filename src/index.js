c684b132fcc2319031b86036677bb26793e3d357
Frank Simon <f.m.sim@t-online.de>
2022-02-20 15:08:19 +0100

    initial commit, react starter template deleted, index.js and index.css installed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDom from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// CSS
+import "./index.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Book" />
+const Title = () => <h1>I Love You To The Moon And Back</h1>
+const Author = () => <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>Amelia Hepworth</h4>
+
+ReactDom.render(<BookList />, document.getElementById("root"))
