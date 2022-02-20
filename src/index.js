f7b544501ab38d4d41c54cfcae492d0a58d10466
Frank Simon <f.m.sim@t-online.de>
2022-02-20 19:57:54 +0100

    finishing booklist, added books.js and Book.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,20 @@
 import React from "react"
 import ReactDom from "react-dom"
 
+import { books } from "./books"
+import Book from "./Book"
+
 // CSS
 import "./index.css"
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map(book => {
+        return <Book key={book.id} {...book} />
+      })}
     </section>
   )
 }
-
-const Book = () => {
-  return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  )
-}
-
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Book" />
-const Title = () => <h1>I Love You To The Moon And Back</h1>
-const Author = () => <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>Amelia Hepworth</h4>
 
 ReactDom.render(<BookList />, document.getElementById("root"))
